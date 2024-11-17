5ad795aef37b088ab6d5b3479044126d52d96121
eam2004110 <e.a.m200411x@gmail.com>
2024-11-17 23:58:30 +0100

    Updated

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const server = createServer(async (req, res) => {
 });
 
 // Server listens on a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

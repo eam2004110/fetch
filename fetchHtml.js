8c8ae9750929735cbb1f3bb30e63fd5b8f62470c
eam2004110 <e.a.m200411x@gmail.com>
2024-11-17 22:48:11 +0100

    Updated

--- fetchHtml.js
+++ fetchHtml.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 import http from "http";
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
